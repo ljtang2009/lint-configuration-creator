83158babea74cd03debc937aaebb12ef708cd50d
Tang Lijin <ho_ho_gl@hotmail.com>
2024-04-29 00:30:57 +0800

    feat: :sparkles: init base code

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+// import meow from 'meow';
+
+// const cli = meow(``, {
+
+// });
+
+// console.log('cli :>> ', cli);
+
+console.log('in create-lint-configuration, args: ', process.argv.slice(2));
